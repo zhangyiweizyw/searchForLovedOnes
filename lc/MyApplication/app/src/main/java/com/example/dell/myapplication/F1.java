bc78058c6b050ffc61bea78c86ec431621df2042
李策 <33644960+occlive@users.noreply.github.com>
2019-11-14 08:21:12 +0800

    lc

---
+++ lc/MyApplication/app/src/main/java/com/example/dell/myapplication/F1.java
@@ -0,0 +1,21 @@
+package com.example.dell.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class F1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View firstpage = inflater.inflate(R.layout.fragment_page1,container,false);
+        firstpage.setBackgroundColor(Color.RED);
+
+        return firstpage;
+    }
+}
---
+++ lc/MyApplication/app/src/main/java/com/example/dell/myapplication/F2.java
@@ -0,0 +1,22 @@
+package com.example.dell.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class F2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View firstpage = inflater.inflate(R.layout.fragment_page2,container,false);
+        firstpage.setBackgroundColor(Color.YELLOW);
+
+        return firstpage;
+    }
+}
