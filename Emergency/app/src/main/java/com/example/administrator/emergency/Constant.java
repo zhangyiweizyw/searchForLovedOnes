7339de97c5ed1ad475de7c68340e41bf919d8bbc
zhangyiweizyw <520071230@qq.com>
2019-12-04 08:38:41 +0800

    webPage2

--- Emergency/app/src/main/java/com/example/administrator/emergency/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.example.administrator.emergency;
-
-public class Constant {
-    public static final String URL = "http://169.254.19.67:8080/";
-    public static final String ADDRESS = "http://169.254.19.67:8080/baidu_ai_test/";
-
-
-}
