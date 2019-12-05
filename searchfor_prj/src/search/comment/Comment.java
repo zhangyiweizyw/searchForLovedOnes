5ed765007d19efb7236b9690293ef6c9f3cf7423
824156591@qq.com <824156591@qq.com>
2019-12-05 08:26:15 +0800

    syd

---
+++ searchfor_prj/src/search/comment/Comment.java
@@ -0,0 +1,70 @@
+package search.comment;
+
+public class Comment {
+	
+	private int id;
+	private String name;
+	private String tel;
+	private String email;
+	private String content;
+	private String qq;
+	private String time;
+	
+	public Comment(int id, String name, String tel, String email, String content, String qq, String time) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.tel = tel;
+		this.email = email;
+		this.content = content;
+		this.qq = qq;
+		this.time = time;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getQq() {
+		return qq;
+	}
+	public void setQq(String qq) {
+		this.qq = qq;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	
+}
