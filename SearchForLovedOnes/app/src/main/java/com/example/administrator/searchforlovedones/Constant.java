424598cff489409debd8eb2f174a9318f18db4ee
李策 <33644960+occlive@users.noreply.github.com>
2019-12-10 15:18:28 +0800

    lc

--- SearchForLovedOnes/app/src/main/java/com/example/administrator/searchforlovedones/Constant.java
+++ SearchForLovedOnes/app/src/main/java/com/example/administrator/searchforlovedones/Constant.java
@@ -2,16 +2,7 @@ package com.example.administrator.searchforlovedones;
 
 public class Constant {
     public static final String BASE_URL="http://10.7.88.183:8080/Xunqin/";
-<<<<<<< HEAD
-//    public static final String IP = "10.7.92.187";
-//    public static final String IP = "10.7.88.27";
-=======
->>>>>>> 610a0add27847c71df0e6a146be1de826657ab87
     public static final String IP = "10.7.88.27";
-
-    /**
-     * by fsl 2019/12/09 19点14分
-     */
     public static final String TEST_URL= "http://10.7.88.82:8080/searchfor_prj/HallSearchServlet";
     public static final String BASE_SERVER="http://10.7.88.82:8080/searchfor_prj/";//加在图片url前的基本地址
 }
