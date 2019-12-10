750abe63fc18cf32265feb6a69d5196cfc63e02c
zhangyiweizyw <520071230@qq.com>
2019-12-10 16:03:34 +0800

    update

--- yh/SearchForProject_User/app/src/main/java/com/software/ashely/searchforproject_user/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.software.ashely.searchforproject_user;
-
-public class Constant {
-    public final static String BASE_IP="http://10.7.88.199:8080/";
-    public final static String BASE_URL ="http://10.7.88.199:8080/SearchForProject_User";
-}
-
