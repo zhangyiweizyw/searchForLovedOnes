a7609761d4d8be601362e5a27d1f299c849c253b
李策 <33644960+occlive@users.noreply.github.com>
2019-12-10 11:08:06 +0800

    lc

--- SearchForLovedOnes/app/src/main/java/com/example/administrator/searchforlovedones/Constant.java
+++ SearchForLovedOnes/app/src/main/java/com/example/administrator/searchforlovedones/Constant.java
@@ -2,7 +2,7 @@ package com.example.administrator.searchforlovedones;
 
 public class Constant {
     public static final String BASE_URL="http://10.7.88.183:8080/Xunqin/";
-    public static final String IP = "10.7.92.184";
+    public static final String IP = "10.7.88.27";
     /**
      * by fsl 2019/12/09 19点14分
      */
